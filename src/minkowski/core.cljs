f82dac3ccd5b782e7f919f0ab1206445faef46d5
Tomasz Pewiński <pewniak747@gmail.com>
2015-04-17 21:05:50 +0200

    draw multiple circles

--- src/minkowski/core.cljs
+++ src/minkowski/core.cljs
@@ -9,14 +9,17 @@
 (def canvas (aget (.getElementsByTagName js/document "canvas") 0))
 (def slider (aget (.getElementsByTagName js/document "input") 0))
 (def drawing (.getContext canvas "2d"))
-(def width 800)
-(def height 800)
+(def unit-pixels 100)
+(def width (* 8 unit-pixels))
+(def height (* 8 unit-pixels))
 (aset canvas "width" width)
 (aset canvas "height" height)
-(defn canvas-point [[x y]] [(+ x (/ width 2)) (+ y (/ height 2))])
+
+(defn canvas-point [[x y]] [(+ (* unit-pixels x) (/ width 2)) (+ (* unit-pixels y) (/ height 2))])
 
 (def radius 30)
 (def draw-points '())
+(def circles [])
 
 (defn angles [n] (range 0 doublepi (/ doublepi n)))
 
@@ -24,17 +27,24 @@
 
 (.addEventListener slider "input" (fn []
   (let [value (aget slider "value")]
-    (set! radius value)
-    (set! draw-points (points 360 radius))
-  )))
+    (set! radius (/ value 100))
+    (set! circles (map (fn [radius] (points 360 radius)) (range 1 4))
+  ))))
+
+(defn draw-circle [circle]
+  (do
+    (.beginPath drawing)
+    (doall (for [point circle] (let [[x y] (canvas-point point)] (.lineTo drawing x y))))
+    (.closePath drawing)
+    (.setLineDash drawing (array 3 5))
+    (aset drawing "strokeStyle" "gray")
+    (.stroke drawing)
+  ))
 
 (defn draw-scene []
   (do
     (.clearRect drawing 0 0 (aget canvas "width") (aget canvas "height"))
-    (.beginPath drawing)
-    (doall (for [point draw-points] (let [[x y] (canvas-point point)] (.lineTo drawing x y))))
-    (.closePath drawing)
-    (.stroke drawing)))
+    (doall (for [circle circles] (draw-circle circle)))))
 
 (defn request-frame []
   (.requestAnimationFrame js/window
