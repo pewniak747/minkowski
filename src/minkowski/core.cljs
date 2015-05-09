6cfd1e58ed06d18fc9c27b04b39fe2fcd4e48072
Tomasz Pewiński <pewniak747@gmail.com>
2015-05-09 20:21:17 +0200

    layout

--- src/minkowski/core.cljs
+++ src/minkowski/core.cljs
@@ -6,6 +6,7 @@
 
 (def doublepi (* 2 Math/PI))
 
+(def visualization (.getElementById js/document "visualization"))
 (def canvas (aget (.getElementsByTagName js/document "canvas") 0))
 (def slider (aget (.getElementsByTagName js/document "input") 0))
 (def drawing (.getContext canvas "2d"))
@@ -17,7 +18,7 @@
 
 (defn canvas-point [[x y]] [(+ (* unit-pixels x) (/ width 2)) (+ (* unit-pixels y) (/ height 2))])
 
-(def radius 30)
+(def radius 0.5)
 (def draw-points '())
 (def circles [])
 
@@ -25,11 +26,25 @@
 
 (defn points [n radius] (map (fn [angle] [(* radius (Math/sin angle)) (* radius (Math/cos angle))]) (angles n)))
 
+(defn window-resized []
+  (let [new-width (aget visualization "clientWidth") new-height (aget visualization "clientHeight")]
+    (set! width new-width)
+    (set! height new-height)
+    (aset canvas "width" width)
+    (aset canvas "height" height)))
+
+(defn recalculate-circles []
+    (set! circles (map (fn [n] (points 360 (* (* 2 n) radius))) (range 1 8))))
+(recalculate-circles)
+
 (.addEventListener slider "input" (fn []
   (let [value (aget slider "value")]
     (set! radius (/ value 100))
-    (set! circles (map (fn [n] (points 360 (* n radius))) (range 1 4))
-  ))))
+    (recalculate-circles)
+  )))
+
+(.addEventListener js/window "resize" window-resized)
+(window-resized)
 
 (defn draw-circle [circle]
   (do
